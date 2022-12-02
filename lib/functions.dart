47bb5e7b9475625bf898fb68c287a686f8a38860
Iyas Muhammed <iyaskottappuram@gmail.com>
2022-12-02 17:34:50 +0530

    Samthul Qibla function finished

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,5 +1,81 @@
 import 'dart:math';
 
+
+double samthulQibla(double aralulBalad, double araluMakka, double thoolulBalad) {
+  // بعد القطر
+  final jaibulAral = sin(aralulBalad * (pi / 180.0));
+  final jaibulMail = sin(araluMakka * (pi / 180.0));
+  final buadulQuthr = jaibulAral * jaibulMail;
+
+//جيب تمام العرض
+
+  final thammuAralulBalad = thamamuAralulbalad(aralulBalad);
+  final jaibuThamamulAral = sin(thammuAralulBalad * (pi / 180.0));
+
+//جيب تمام العرض
+
+  final thamamulMail = 90 - araluMakka;
+  final jaibuThamamulMail = sin(thamamulMail * (pi / 180.0));
+
+  //أصل مطلق
+
+  final aslMuthlaq = jaibuThamamulAral * jaibuThamamulMail;
+
+//جيب تمام ما بين الطولين
+
+  final mabainathoolaini = mabainaTholaini(thoolulBalad);
+  final thamamuMabainathoolaini = thamamuMabainaThoolaini(mabainathoolaini);
+  final jaibuThamamuMabainaThoolaini =
+      sin(thamamuMabainathoolaini * (pi / 180.0));
+
+  //أصل معدل
+
+  final aslMuaddal = aslMuthlaq * jaibuThamamuMabainaThoolaini;
+
+  //جيب الإرتفاع
+
+  final jaibulIrthifa = aslMuaddal + buadulQuthr;
+
+  //قوس الإرتفاع
+
+  final qousulIrthifa = asin(jaibulIrthifa) * (180.0 / pi);
+
+  //حصة السمت
+
+  final hissathuSamth = (jaibulIrthifa * jaibulAral) / jaibuThamamulAral;
+
+  //جيب السعت
+
+  final jaibuSsahath = jaibulMail / jaibuThamamulAral;
+
+  //تعديل السمت
+
+  final thaadeeluSamth = thaadeeluSsamth(hissathuSamth, jaibuSsahath);
+
+  //تمام الإرتفاع
+
+  final thamamulIrthifa = 90 - qousulIrthifa;
+
+  //جيب تمام الإرتفاع
+
+  final jaibuThamamulIrthifa = sin(thamamulIrthifa * (pi / 180.0));
+
+  //جيب السمت
+
+  final jaibuSamth = thaadeeluSamth / jaibuThamamulIrthifa;
+
+  //قوس السمت
+
+  final qousuSamth = asin(jaibuSamth) * (180.0 / pi);
+  return qousuSamth;
+}
+
+
+
+
+
+
+
 void splitThoolAndAral(double thoolOrAral) {
   final daraja = thoolOrAral.toInt();
   final darajaAfterDecimal = thoolOrAral - daraja;
@@ -43,40 +119,12 @@ double mabainalAralaini(double aralulBalad) {
   }
 }
 
-void samthulQibla(double aralulBalad, double araluMakka, double thoolulBalad) {
-  // بعد القطر
-  final jaibulAral = sin(aralulBalad)*(pi/180.0);
-  final jaibulMail = sin(araluMakka)*(pi/180.0);
-  final buadulQuthr = jaibulAral * jaibulMail;
-
-//جيب تمام العرض
-
-  final thammuAralulBalad = thamamuAralulbalad(aralulBalad);
-  final jaibuThamamulAral = sin(thammuAralulBalad)*(pi/180.0);
-
-//جيب تمام العرض
-
-  final thamamulMail = 90 - araluMakka;
-  final jaibuThamamulMail = sin(thamamulMail)*(pi/180.0);
-
-  //أصل مطلق
-
-  final aslMuthlaq = jaibuThamamulAral * jaibuThamamulMail;
-
-//جيب تمام ما بين الطولين
-
-  final mabainathoolaini = mabainaTholaini(thoolulBalad);
-  final thamamuMabainathoolaini = thamamuMabainaThoolaini(mabainathoolaini);
-  final jaibuThamamuMabainaThoolaini = sin(thamamuMabainathoolaini)*(pi/180.0);
-
-  //أصل معدل
-
-  final aslMuaddal = aslMuthlaq * jaibuThamamuMabainaThoolaini;
-
-  //جيب الإرتفاع
-
-  final jaibulIrthifa = aslMuaddal + buadulQuthr;
-
-  //قوس الإرتفاع
-  
-  final qousulIrthifa = asin(jaibulIrthifa) * (180.0 / pi);}
+double thaadeeluSsamth(double hissathuSamth, double jaibuSsahath) {
+  if (hissathuSamth >= jaibuSsahath) {
+    final thaadeeluSsamth = hissathuSamth - jaibuSsahath;
+    return thaadeeluSsamth;
+  } else {
+    final thaadeeluSsamth = jaibuSsahath - hissathuSamth;
+    return thaadeeluSsamth;
+  }
+}
